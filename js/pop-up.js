f9b4ae48113b95435738ef2188313ce0404f6d0e
NightRoadMan <33252789+NightRoadMan@users.noreply.github.com>
2017-12-02 23:34:13 +0300

    Проблемы с JS

--- js/pop-up.js
+++ js/pop-up.js
@@ -1,20 +1,35 @@
 	var link = document.querySelector(".button-popup");
 	var popup = document.querySelector(".pop-up");
-	var close = popup.querySelector(".button-popup");
-	var like = popup.querySelector("[class=pop-up-open]");
-	var butt = popup.querySelector("[name=figara]");
+	var form = popup.querySelector("form");
 
-	if (butt==like) {
-	close.addEventListener("click", function (evt) {
-    evt.preventDefault();
-    popup.classList.remove("pop-up-open");
-  });
-	}
-	else {
+	var timeIn = popup.querySelector("[name=comein]");
+	var timeOut = popup.querySelector("[name=comeout]");
+
+	var storage = localStorage.getItem("timeIn");
+	
+
+
 	link.addEventListener("click", function (evt) {
 	evt.preventDefault();
-	popup.classList.add("pop-up-open");
+	popup.classList.toggle("pop-up-close");
 	});
+
+
+
+	form.addEventListener("submit", function (evt) {
+	if (!timeIn.value || !timeOut.value) {
+	evt.preventDefault();
+	console.log("Нужно ввести логин и пароль");
+	} else {
+	localStorage.setItem("comein", timeIn.value);
+	}
+	});
+
+	window.addEventListener("keydown", function (evt) {
+		if (evt.keyCode === 27) {
+			if (!form.classList.contains("pop-up-close") == false) {
+				form.classList.contains("pop-up-close");
+			}
 	}
 
-
+	});
