333d1eed6dc32f760cf82f1f8acf4c2064eb21e5
NightRoadMan <33252789+NightRoadMan@users.noreply.github.com>
2017-12-06 22:54:07 +0300

    Минимизация JS

--- js/pop-up.js
+++ js/pop-up.js
@@ -1,27 +1,23 @@
 	var link = document.querySelector(".button-popup");
 	var popup = document.querySelector(".pop-up");
-	
-
 	var timeIn = popup.querySelector("[name=comein]");
 	var timeOut = popup.querySelector("[name=comeout]");
-
 	var storage = localStorage.getItem("timeIn");
 	var storage = localStorage.getItem("timeOut");
-	
-	popup.classList.add("pop-up-js-init");
 
+	popup.classList.add("pop-up-js-init");
 	link.addEventListener("click", function (evt) {
 	evt.preventDefault();
-		if (storage) {
+	if (storage) {
 		timeIn.value = storage;
-			setTimeout(function () {
+		setTimeout(function () {
 			timeOut.focus();
-			}, 700);
-    } else {
-			setTimeout(function () {
+		}, 700);
+	} else {
+		setTimeout(function () {
 			timeIn.focus();
-			}, 700);
-    }
+		}, 700);
+	}
 
 	if (popup.classList.contains("pop-up-js-init")) {
 		popup.classList.remove("pop-up-js-init");
@@ -36,18 +32,17 @@
 	});
 
 	window.addEventListener("keydown", function (evt) {
-		if (evt.keyCode === 27) {
-			console.log("111")
+		if (evt.keyCode === 27) {			
 			if (popup.classList.contains("pop-up-visible")) {
 				popup.classList.add("pop-up-hide");
 				popup.classList.remove("pop-up-visible");
 				popup.classList.remove("modal-error");
 			}
-	}
+		}
 	});
 
 	popup.addEventListener("submit", function (evt) {
-	    if (!timeIn.value || !timeOut.value) {
+		if (!timeIn.value || !timeOut.value) {
 			evt.preventDefault();			
 			popup.classList.remove("modal-error");
 			popup.offsetWidth = popup.offsetWidth;
